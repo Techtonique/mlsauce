0aef5fb553fc75c72c89809499e515e3d25e04c9
Thierry Moudiki <thierry.moudiki@gmail.com>
2020-05-08 17:47:41 +0200

    Bump version: 0.2.0 → 0.2.1

--- mlsauce/__init__.py
+++ mlsauce/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """T. Moudiki"""
 __email__ = "thierry.moudiki@gmail.com"
-__version__ = "__version__ = '0.2.0'"
+__version__ = "__version__ = '0.2.1'"
 
 from .adaopt.adaopt import AdaOpt
 
