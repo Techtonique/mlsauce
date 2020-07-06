93e9fadd3cd990cf432410aa5676e3f1e76aaa78
Thierry Moudiki <thierry.moudiki@gmail.com>
2020-07-06 17:09:43 +0200

    update example

--- examples/adaopt_classifier.py
+++ examples/adaopt_classifier.py
@@ -1,5 +1,4 @@
 import numpy as np 
-import pandas as pd
 from sklearn.datasets import load_digits, load_breast_cancer, load_wine, load_iris
 from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
 from time import time
