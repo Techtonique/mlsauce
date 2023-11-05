f7e91d928209bfe48f242feacf4fe175365b0061
Thierry Moudiki <thierry.moudiki@gmail.com>
2023-11-05 11:34:58 +0100

    update example

--- examples/ridge_regressor.py
+++ examples/ridge_regressor.py
@@ -1,5 +1,5 @@
 import numpy as np 
-from sklearn.datasets import load_boston, load_diabetes
+from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
 from time import time
 from os import chdir
@@ -13,33 +13,6 @@ from sklearn import metrics
 import mlsauce as ms
 
 # ridge
-
-# data 1
-boston = load_boston()
-X = boston.data
-y = boston.target
-# split data into training test and test set
-np.random.seed(15029)
-X_train, X_test, y_train, y_test = train_test_split(X, y, 
-                                                    test_size=0.2)
-
-obj = ms.RidgeRegressor(backend="cpu")
-start = time()
-obj.fit(X_train, y_train)
-print(time()-start)
-start = time()
-print(np.sqrt(np.mean(np.square(obj.predict(X_test) - y_test))))
-print(time()-start)
-
-obj = ms.RidgeRegressor(backend="gpu")
-start = time()
-obj.fit(X_train, y_train)
-print(time()-start)
-start = time()
-print(np.sqrt(np.mean(np.square(obj.predict(X_test) - y_test))))
-print(time()-start)
-print(obj.beta)
-
 
 # data 2
 diabetes = load_diabetes()
