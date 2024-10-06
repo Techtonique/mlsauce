aad3ad2e153a9fee80e1412f88f4d017dfc8360c
Thierry Moudiki <thierry.moudiki@gmail.com>
2024-10-06 04:22:28 +0000

    update version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ MAINTAINER = 'T. Moudiki'
 MAINTAINER_EMAIL = 'thierry.moudiki@gmail.com'
 LICENSE = 'BSD3 Clause Clear'
 
-__version__ = '0.20.1'
+__version__ = '0.20.2'
 
 VERSION = __version__
 
