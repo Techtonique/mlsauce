e278f38a17d7e6a1522aa556d75912370dcd6b62
Thierry Moudiki <thierry.moudiki@gmail.com>
2024-10-28 08:40:46 +0100

    modif gethistofeatures

--- mlsauce/utils/histofeatures/gethistofeatures.py
+++ mlsauce/utils/histofeatures/gethistofeatures.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 
+
 def create_histogram_with_bin_values(x):
     """
     Computes a histogram for the input data and assigns a value to each bin
@@ -15,48 +16,43 @@ def create_histogram_with_bin_values(x):
     bin_value_dict (dict): A dictionary where keys are the bin ranges (tuples) and values reflect the ordering.
     """
     # Compute the histogram
-    hist, bin_edges = np.histogram(x, bins="auto")
+    _, bin_edges = np.histogram(x, bins="auto")
 
-    bin_edges = np.concatenate([[-1e10], bin_edges, [1e10]]).ravel()
-    
-    # Create a dict to store bin ranges and assigned values
-    bin_value_dict = {}
-
-    for i in range(len(bin_edges) - 1):
-        bin_range = (bin_edges[i], bin_edges[i + 1])
-        if bin_edges[i] == -1e10:
-            bin_value_dict[i] = (bin_range, bin_edges[i + 1])
-        elif bin_edges[i + 1] == 1e10:
-            bin_value_dict[i] = (bin_range, bin_edges[i])
-        else: 
-            bin_value_dict[i] = (bin_range, np.median(list(bin_range)))
+    bin_edges = np.concatenate([[-1e10], bin_edges, [1e10]]).ravel()        
         
-    return bin_edges, bin_value_dict
+    return {i: (bin_edges[i], bin_edges[i + 1]) for i in range(len(bin_edges) - 1)}
 
-def assign_values_to_input(new_data, bin_value_dict):
+
+def assign_values_to_input(x, bin_value_dict):
     """
     Assigns values to a new input based on the provided bin ranges and values.
 
     Args:
-    new_data (list or np.array): New input data to assign values to.
+    x (list or np.array): New input data to assign values to.
     bin_value_dict (dict): Dictionary where keys are bin ranges (tuples) and values are the assigned values.
 
     Returns:
     assigned_values (list): List of assigned values for the new input data.
     """
+
+    if np.issubdtype(x.dtype, np.integer) or np.issubdtype(x.dtype, np.object_):
+
+        return x
+
     assigned_values = []
 
-    for value in new_data:
+    for value in x:
         assigned = None
         # Find the appropriate bin for each value
-        for elt in bin_value_dict.items():
-            if elt[1][0][0] < value <= elt[1][0][1]:
-                assigned = elt[1][1]
+        for i, elt in enumerate(bin_value_dict.items()):
+            if elt[1][0] < value <= elt[1][1]:
+                assigned = i
                 break
 
         assigned_values.append(assigned)
 
     return np.asarray(assigned_values).ravel()
+
 
 def get_histo_features(X, bin_value_dict=None):
     """
@@ -77,12 +73,12 @@ def get_histo_features(X, bin_value_dict=None):
             X_hist = pd.DataFrame(np.zeros(X.shape), 
                                         columns=colnames)
             for i, col in enumerate(colnames):
-                _, bin_value_dict = create_histogram_with_bin_values(X[:, i])
+                bin_value_dict = create_histogram_with_bin_values(X[:, i])
                 X_hist[col] = assign_values_to_input(X[:, i], bin_value_dict)
         else: 
             X_hist = np.zeros(X.shape)        
             for i in range(X.shape[1]):
-                _, bin_value_dict = create_histogram_with_bin_values(X[:, i])
+                bin_value_dict = create_histogram_with_bin_values(X[:, i])
                 X_hist[:, i] = assign_values_to_input(X[:, i], bin_value_dict)            
             
         return X_hist, bin_value_dict
