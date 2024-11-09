a28eaea5c67bc221f3739035ac039de0badca85a
Thierry Moudiki <thierry.moudiki@gmail.com>
2024-11-09 18:33:21 +0000

    get_beta for GPU

--- mlsauce/utils/get_beta.py
+++ mlsauce/utils/get_beta.py
@@ -1,5 +1,14 @@
 import numpy as np
+try:
+    import jax.numpy as jnp
+    from jax import device_put
+    from jax.numpy.linalg import inv as jinv
+except ImportError:
+    pass
 
-
-def get_beta(X, y):
-    return np.linalg.solve(X.T @ X, X.T @ y)
+def get_beta(X, y, backend="cpu"):
+    if backend == "cpu":
+        return np.linalg.solve(X.T @ X, X.T @ y)
+    device_put(X)
+    device_put(y)    
+    return jnp.linalg.solve(X.T @ X, X.T @ y)
